9a54800e1ae5d35d4e76669ba91cca301256f8f7
Joao Gabriel Cabral Rampasso <113752824+Bolojaka@users.noreply.github.com>
2023-10-18 22:52:59 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,20 @@
+<html>
+  <head>
+    <title>PHP Test</title>
+  </head>
+  <body>
+   <h1>teste</h1>
+    <form action="media.php" method="get">
+      <label for="idN1">Digite Nota1 </label>
+      <br>
+      <input type="text" id="idN1" name="nota1">
+      <br>
+      <label for="idN2">Digite Nota2 </label>
+      <br>
+      <input type="text" id="idN2" name="nota2">
+      <br><br>
+      <input type="submit" value="Calcular">      
+    </form>
+  
+  </body>
+</html>
